3d22f194918bd0c3952aeef8c86b6fb5f5bd2784
105841104421-sari-labweb <105841104421@student.unismuh.ac.id>
2024-06-14 12:39:59 +0800

    Tugas 2

--- App.js
+++ App.js
@@ -5,33 +5,27 @@ const App = () => {
   return (
     <View style={{
       flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
     }}>
-      <View style={{
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-      }}>
       <View style={{
         width: 100,
         height: 100,
         backgroundColor: 'red',
+        alignItems: 'center',
       }}>
-      </View>
+        <Text>App</Text>
     </View>
-    <View style={{
-      flex: 1,
-      justifyContent: 'flex-end',
-      alignItems: 'flex-end',
-    }}>
       <View style={{
         width: 100,
         height: 100,
         backgroundColor: 'blue',
+        alignSelf: 'center',
       }}>
+        <Text>App</Text>
       </View>
       </View>
-    </View>
+    
   )
 }
-export default App
-const styles = StyleSheet.create({})
+export default App;
