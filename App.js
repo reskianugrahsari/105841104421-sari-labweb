a0e89dda84816081682e63f205dda0f11e5eca55
105841104421-sari-labweb <105841104421@student.unismuh.ac.id>
2024-06-24 16:11:17 +0800

    button costum dan text input

--- App.js
+++ App.js
@@ -1,56 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 
-const App = () => {
+//Costum component,ButtonCostum
+const BottonCostum=({text,color})=> {
   return (
-    <View style={{
-      flex:1,
-      flexDirection:'row',
+    <View>
+    <View style ={{
+      widht:250,
+      height:100,
+      backgroundColor:color,
+      borderRadius:10,
+      borderBottom:20,
+      justifyContent:'center',
+      alignitems:'center',
     }}>
-      <View style={{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'flex-end',
-        marginLeft:100,
-      }}>
-        <View style={{
-          widht:100,
-          height:50,
-          backgroundColor:'red',
-          borderRadius:8,
-          justifyContent:'center',
-        }}>
-          <Text style={{
-            fontSize:20,
-            color:'white',
-            textAlign:'center',
-            fontWeight:'bold',
-          }}>SingIn</Text>
-        </View>
-      </View>
-      <View style={{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'flex-end',
-        marginRight:100,
-      }}>
-        <View style={{
-          widht:100,
-          height:50,
-          backgroundColor:'blue',
-          borderRadius:8,
-          justifyContent:'center'
-        }}>
-          <Text style={{
-            fontSize:20,
-            color:'white',
-            textAlign:'center',
-            fontWeight:'bold',
-          }}>SingUp</Text>
-        </View>
-      </View>
+      <Text style={{
+        textAlign:'center',
+        color:'white',
+        fontSize:30,
+        fontWeight:'bold',
+      }}>{text}
+      </Text>
     </View>
-    
+    </View>
   )
 }
-export default App
+//costum component,TextInputCustom
+const TextInputCostum=({placeholder,color,typeKeyboard})=>{
+  return(
+    <TextInput
+    keyboardType={typeKeyboard}
+    placeholder={placeholder}
+    style={{
+      width:250,
+      height:50,
+      borderColor:color,
+      borderWidth:3,
+      borderRadius:5,
+      marginBottom:10,
+      paddingLeft:10,
+    }}/>
+
+  )
+}
+
+export default function App(){
+  return(
+    <View style={{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+    }}>
+      <BottonCostum text="LOGIN" color="blue"/>
+      <BottonCostum text="SIGN IN" color="green"/>
+      <BottonCostum text="REGISTER" color="red"/>
+      <TextInputCostum placeholder="Enter text" color="pink" typeKeyboard="default"/>
+      <TextInputCostum placeholder="Enter number" color="yellow" typeKeyboard="numeric"/>
+
+    </View>
+  )
+}
